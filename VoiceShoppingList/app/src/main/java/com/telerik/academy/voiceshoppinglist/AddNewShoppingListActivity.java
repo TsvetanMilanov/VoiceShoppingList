f906078f5f81bd8ebeb25672631bd60128ba9a70
todorm85 <todorbm85@gmail.com>
2016-01-09 17:41:26 +0200

    Add shopping list functionality
    Items can be added to the UI in a vertical list via button tap. The view automatically scrolls to the last one added when it overflows.

--- VoiceShoppingList/app/src/main/java/com/telerik/academy/voiceshoppinglist/AddNewShoppingListActivity.java
+++ VoiceShoppingList/app/src/main/java/com/telerik/academy/voiceshoppinglist/AddNewShoppingListActivity.java
@@ -3,13 +3,38 @@ package com.telerik.academy.voiceshoppinglist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class AddNewShoppingListActivity extends AppCompatActivity {
+
+    private LinearLayout productsList;
+    private ScrollView mainScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_shopping_list);
+
+        productsList = (LinearLayout) findViewById(R.id.productsList);
+        mainScrollView = (ScrollView) findViewById(R.id.mainScrollView);
+    }
+
+    public void onAddBtnClick(View view) {
+        EditText newItem = new EditText(this);
+        productsList.addView(newItem);
+
+        mainScrollView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mainScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        }, 200);
+
+        Toast.makeText(this, "Item added", Toast.LENGTH_SHORT).show();
     }
 
     @Override
